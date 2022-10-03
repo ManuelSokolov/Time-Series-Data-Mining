69f60e638bf3bf7b620c58f0bf639170250f7722
ist1102142 <ist1102142@tecnico.ulisboa.pt>
2022-10-03 16:42:32 +0200

    create dirs automatically

--- Titanic_Dataset/Main.py
+++ Titanic_Dataset/Main.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import Data_Analysis
+import os
 
 if __name__ == '__main__':
-    dataset = pd.read_csv('train.csv')
-    da = Data_Analysis.Data_Analysis(dataset)
-    da.data_profiling()
+	newpath = r'Data_Profiling' 
+	newpath2 = r'Data_Profiling/Distribution' 
+if not os.path.exists(newpath):
+	os.makedirs(newpath)
+if not os.path.exists(newpath2):
+	os.makedirs(newpath2)
+
+dataset = pd.read_csv('train.csv')
+da = Data_Analysis.Data_Analysis(dataset)
+da.data_profiling()
